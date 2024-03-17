7ab9b79e464371257478759dd763919e9592fb8f
Nicolas <nicolas@dlpbasculas.com>
2024-03-16 20:43:50 -0500

    Update tsconfig.json, astro.config.mjs, package.json, and tailwind.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+import react from "@astrojs/react";
+
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(
+    {
+      applyBaseStyles: false,
+      nesting: true
+    }
+  ), react()]
 });
