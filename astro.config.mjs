af95c955cfb769a3e5d6f5d91ee63109e30b896b
Nicolas <nicolas@dlpbasculas.com>
2024-03-17 14:13:50 -0500

    Config Astro without React, main page donde.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,4 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
-
-export default defineConfig({
-  integrations: [tailwind(
-    {
-      applyBaseStyles: false,
-      nesting: true
-    }
-  ), react()]
-});
+// https://astro.build/config
+export default defineConfig({});
